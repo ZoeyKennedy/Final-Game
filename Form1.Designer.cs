d9af6e32f3c812ed8d12814cb6f1c2e4cde26898
Zoey Kennedy <339523656@GoTVDSB.ca>
2024-06-07 14:36:22 -0400

    ?

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,28 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.imgTop = new System.Windows.Forms.PictureBox();
+            this.imgBottom = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imgTop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBottom)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // imgTop
+            // 
+            this.imgTop.BackColor = System.Drawing.Color.Transparent;
+            this.imgTop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.imgTop.Location = new System.Drawing.Point(473, 81);
+            this.imgTop.Name = "imgTop";
+            this.imgTop.Size = new System.Drawing.Size(206, 152);
+            this.imgTop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgTop.TabIndex = 2;
+            this.imgTop.TabStop = false;
+            this.imgTop.Click += new System.EventHandler(this.imgTop_Click);
+            // 
+            // imgBottom
+            // 
+            this.imgBottom.BackColor = System.Drawing.Color.Transparent;
+            this.imgBottom.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.imgBottom.Location = new System.Drawing.Point(67, 174);
+            this.imgBottom.Name = "imgBottom";
+            this.imgBottom.Size = new System.Drawing.Size(223, 169);
+            this.imgBottom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgBottom.TabIndex = 1;
+            this.imgBottom.TabStop = false;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Final_Game.Properties.Resources._base;
-            this.pictureBox1.Location = new System.Drawing.Point(157, 43);
+            this.pictureBox1.Location = new System.Drawing.Point(136, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(412, 409);
+            this.pictureBox1.Size = new System.Drawing.Size(449, 449);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // btnButton
+            // 
+            this.btnButton.Location = new System.Drawing.Point(27, 53);
+            this.btnButton.Name = "btnButton";
+            this.btnButton.Size = new System.Drawing.Size(103, 65);
+            this.btnButton.TabIndex = 3;
+            this.btnButton.Text = "button1";
+            this.btnButton.UseVisualStyleBackColor = true;
+            this.btnButton.Click += new System.EventHandler(this.btnButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(725, 487);
+            this.Controls.Add(this.btnButton);
+            this.Controls.Add(this.imgTop);
+            this.Controls.Add(this.imgBottom);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgTop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBottom)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -58,6 +102,9 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox imgBottom;
+        private System.Windows.Forms.PictureBox imgTop;
+        private System.Windows.Forms.Button btnButton;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Final_Game.Properties;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,30 @@ namespace Final_Game
 {
     public partial class Form1 : Form
     {
+        
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void imgTop_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void btnButton_Click(object sender, EventArgs e)
+        {
+             imgTop.Image = Properties.Resources.top1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = Color.Transparent;
+
+            // Load the image with a transparent background
+            
+        } 
     }
 }
